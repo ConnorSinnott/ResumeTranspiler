8ac612df4d2f7394904d011f3372db89bf3bdce8
Connor Sinnott <connorsinnott@gmail.com>
2019-12-28 16:48:27 -0800

    Created a basic 'Hello World' express application

--- builder/src/main.ts
+++ builder/src/main.ts
@@ -1,3 +1,15 @@
-const message : string = 'Hello World';
+import express from 'express';
 
-console.log(message);
+const SERVER_PORT = process.env['server_port'];
+if (!SERVER_PORT)
+    throw new Error('Environment variable "server_port" not defined');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}`);
+});
