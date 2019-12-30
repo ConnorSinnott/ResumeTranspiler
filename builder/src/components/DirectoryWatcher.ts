ced9649b1c73edd4860ed527184b1f33c95c157b
Connor Sinnott <connorsinnott@gmail.com>
2019-12-29 16:58:15 -0800

    Chokidar now watches for changes in resume

--- builder/src/components/DirectoryWatcher.ts
+++ builder/src/components/DirectoryWatcher.ts
@@ -1,1 +1,7 @@
-export const watchDirectory = (directory: string, onChange: () => void) => {};
+import chokidar from 'chokidar';
+
+export const watchDirectory = (directory: string, onChange: () => void) => {
+    chokidar.watch(directory).on('all', (event, path) => {
+        onChange();
+    });
+};
