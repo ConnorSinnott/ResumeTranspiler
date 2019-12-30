e6f06c9c552045ad49a3f7efa2db1df495ef7044
Connor Sinnott <connorsinnott@gmail.com>
2019-12-29 16:25:30 -0800

    Renderer now renders pug

--- builder/src/components/Renderer.ts
+++ builder/src/components/Renderer.ts
@@ -1,1 +1,8 @@
-export const render = (sourceDirectory: string, entryPoint: string) => {};
+import pug from 'pug';
+import path from 'path';
+
+export const render = (sourceDirectory: string, entryPoint: string) => {
+    const targetFile = path.join(sourceDirectory, entryPoint);
+
+    return pug.renderFile(targetFile);
+};
