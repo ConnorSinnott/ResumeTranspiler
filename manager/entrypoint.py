6f3653359ef7c0caf5bcad6f5c60d6c8466db005
Connor Sinnott <connorsinnott@gmail.com>
2020-01-03 11:23:44 -0800

    Manager can now list folders

--- manager/entrypoint.py
+++ manager/entrypoint.py
@@ -1,7 +1,13 @@
 import sys
+import os
+
+def _getDirectories():
+    stream = os.popen('aws s3 ls s3://resumetranspiler | grep -Eo "\d{2}-\d{2}-\d{4}"')
+    result = stream.read().strip()
+    return result.split('\n')
 
 def listDirectories():
-    print 'Listing directories'
+    print '\n'.join(_getDirectories())
 
 def checkoutDirectory(arguments):
     if len(arguments) < 1:
