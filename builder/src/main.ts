79f4bb7f11790e9b18b54514d47973ed51f592e5
Connor Sinnott <connorsinnott@gmail.com>
2020-01-05 15:22:40 -0800

    Ports are now hard-coded. Customization will be done during binding

--- builder/src/main.ts
+++ builder/src/main.ts
@@ -5,14 +5,13 @@ const SOURCE_DIRECTORY = process.env['source_directory'];
 if (!SOURCE_DIRECTORY)
     throw new Error('Environment variable "source_directory not defined');
 
-const SERVER_PORT = process.env['server_port'];
-if (!SERVER_PORT)
-    throw new Error('Environment variable "server_port" not defined');
+const SERVER_PORT = 3000;
+const RELOAD_SERVER_PORT = 3001;
 
 startDevelopmentServer({
-    developmentServerPort: parseInt(SERVER_PORT),
+    developmentServerPort: SERVER_PORT,
     sourceDirectory: SOURCE_DIRECTORY,
-    reloadServerPort: 3001,
+    reloadServerPort: RELOAD_SERVER_PORT,
 }).then(() => {
     Logger.info(`Server listening on port ${SERVER_PORT}`);
 });
