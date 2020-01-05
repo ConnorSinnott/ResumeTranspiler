3640feba96dd1da137e70f0109812ac00fe38935
Connor Sinnott <connorsinnott@gmail.com>
2020-01-05 12:41:04 -0800

    Reformatted resumetranspiler.sh for multi-line conditions

--- resumetranspiler.sh
+++ resumetranspiler.sh
@@ -8,13 +8,34 @@ case $1 in
 
     docker-compose up -d builder
     ;;
-  stop-dev) docker-compose down -v;;
-  status) docker-compose ps;;
-  list) docker-compose run --rm manager list;;
-  push) docker-compose run --rm manager push;;
-  checkout) docker-compose run --rm -e HOST_USER_ID=$(id -u $USER) manager checkout $2;;
-  delete) docker-compose run --rm manager delete $2;;
+
+  stop-dev)
+    docker-compose down -v
+    ;;
+
+  status)
+    docker-compose ps
+    ;;
+
+  list)
+    docker-compose run --rm manager list
+    ;;
+
+  push)
+    docker-compose run --rm manager push
+    ;;
+
+  checkout)
+    remoteDirectoryName=$2
+
+    docker-compose run --rm -e HOST_USER_ID=$(id -u $USER) manager checkout $remoteDirectoryName
+    ;;
+
+  delete)
+    docker-compose run --rm manager delete $2
+    ;;
+
   *)
     echo "Invalid command $1"
-  ;;
+    ;;
 esac
